ff87ddd8a93708ccfee189afe86d98e25f0c5ad0
mascot6699 <umangshucool@gmail.com>
2019-12-27 12:02:02 +0000

    [BUG] relative path changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { CruxScreen } from "./crux-ui/crux-screen";
 import * as CruxUIUtil from "./crux-ui/utils";
-import * as RNCruxUI from "dist/crux-ui.native";
+import * as RNCruxUI from "../dist/crux-ui.native";
 
 export {
     CruxUIUtil,
